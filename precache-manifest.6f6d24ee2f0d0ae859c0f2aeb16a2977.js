274267499c31a238efa1c7963e3cc29540f2a0ef
liaoc <18211481861@163.com>
2019-03-23 13:10:04 +0800

    init project

---
+++ precache-manifest.6f6d24ee2f0d0ae859c0f2aeb16a2977.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6167687fcbd63fe9dca50ffd44a2bff3",
+    "url": "/shared-library-ui/static/media/word_l.6167687f.png"
+  },
+  {
+    "revision": "1823c2a3cfb5ffa718bd",
+    "url": "/shared-library-ui/static/css/main.d94e1b6d.chunk.css"
+  },
+  {
+    "revision": "02f4269a472db8747d9f5a492758db45",
+    "url": "/shared-library-ui/static/media/logo.02f4269a.png"
+  },
+  {
+    "revision": "6e55aa57ee22d790d857",
+    "url": "/shared-library-ui/static/js/1.6e55aa57.chunk.js"
+  },
+  {
+    "revision": "78c3d17d800ab2c26f17",
+    "url": "/shared-library-ui/static/js/runtime~main.78c3d17d.js"
+  },
+  {
+    "revision": "5caf7bfcd6710104a42154534fad07af",
+    "url": "/shared-library-ui/static/media/pdf_s.5caf7bfc.png"
+  },
+  {
+    "revision": "1e078ab284ddf5ee8b213aa0dc92167a",
+    "url": "/shared-library-ui/static/media/word_s.1e078ab2.png"
+  },
+  {
+    "revision": "1823c2a3cfb5ffa718bd",
+    "url": "/shared-library-ui/static/js/main.1823c2a3.chunk.js"
+  },
+  {
+    "revision": "ae4e37a88fb9d386101022770886b808",
+    "url": "/shared-library-ui/static/media/pdf_l.ae4e37a8.png"
+  },
+  {
+    "revision": "edd2a89cdf2ee0518be0a87c3b23f101",
+    "url": "/shared-library-ui/static/media/banner1.edd2a89c.jpg"
+  },
+  {
+    "revision": "6f240434afe2745189d30c93017140b2",
+    "url": "/shared-library-ui/static/media/banner2.6f240434.jpg"
+  },
+  {
+    "revision": "0d4920cece20634af0039a784c64bbda",
+    "url": "/shared-library-ui/static/media/banner3.0d4920ce.jpg"
+  },
+  {
+    "revision": "bf0cfeac0b7babec318cc0887bb396fa",
+    "url": "/shared-library-ui/static/media/cover-news.bf0cfeac.png"
+  },
+  {
+    "revision": "6e55aa57ee22d790d857",
+    "url": "/shared-library-ui/static/css/1.e36ac706.chunk.css"
+  },
+  {
+    "revision": "786b6459de7b87afc74f4b12b5bfd3a2",
+    "url": "/shared-library-ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/shared-library-ui/precache-manifest.6f6d24ee2f0d0ae859c0f2aeb16a2977.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/shared-library-ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
